a3b888ab064089502d7772e8877fbcc3629db951
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-05-19 16:55:26 +0300

    implement 1st task

--- 1.Multithreading in .NET/MultiThreading.Task1.100Tasks/Program.cs
+++ 1.Multithreading in .NET/MultiThreading.Task1.100Tasks/Program.cs
@@ -4,6 +4,7 @@
  * “Task #0 – {iteration number}”.
  */
 using System;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task1._100Tasks
 {
@@ -19,7 +20,7 @@ namespace MultiThreading.Task1._100Tasks
             Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
             Console.WriteLine("“Task #0 – {iteration number}”.");
             Console.WriteLine();
-            
+
             HundredTasks();
 
             Console.ReadLine();
@@ -27,12 +28,22 @@ namespace MultiThreading.Task1._100Tasks
 
         static void HundredTasks()
         {
-            // feel free to add your code here
+            var taskArray = new Task[TaskAmount];
+            for (var i = 0; i < taskArray.Length; i++)
+            {
+                var taskNumber = i;
+                taskArray[i] = Task.Factory.StartNew(() => Output(taskNumber));
+            }
+
+            Task.WaitAll(taskArray);
         }
 
-        static void Output(int taskNumber, int iterationNumber)
+        private static void Output(int taskNumber)
         {
-            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
+            for (var i = 0; i < MaxIterationsCount; i++)
+            {
+                Console.WriteLine($"Task #{taskNumber} – {i}");
+            }
         }
     }
 }
