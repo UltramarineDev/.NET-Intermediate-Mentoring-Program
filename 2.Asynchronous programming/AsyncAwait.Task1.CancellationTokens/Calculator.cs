33e1bb1114ebe54745795b1995df7ed8882b0423
MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>
2021-06-18 13:56:14 +0300

    add module 2

---
+++ 2.Asynchronous programming/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens
+{
+    public static class Calculator
+    {
+        public static Task<long> Calculate(int n, CancellationToken cancellationToken = default)
+        {
+            long sum = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                   cancellationToken.ThrowIfCancellationRequested();
+                }
+
+                // i + 1 is to allow 2147483647 (Max(Int32)) 
+                sum += (i + 1);
+                Thread.Sleep(10);
+            }
+
+            return Task.FromResult(sum);
+        }
+    }
+}
