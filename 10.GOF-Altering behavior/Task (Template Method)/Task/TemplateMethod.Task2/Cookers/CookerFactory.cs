12ce0e7417531f5839f7544264464d382fc7774a
UltramarineDev <43013208+UltramarineDev@users.noreply.github.com>
2021-08-27 17:21:46 +0300

    implement task 2 (#10)
    Co-authored-by: MINSK\Maryna_Sakalova <maryna_sakalova@epam.com>

---
+++ 10.GOF-Altering behavior/Task (Template Method)/Task/TemplateMethod.Task2/Cookers/CookerFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace TemplateMethod.Task2.Cookers
+{
+    public class CookerFactory
+    {
+        public CookerBase CreateUkraineCooker() => new UkraineCooker();
+        public CookerBase CreateIndianCooker() => new IndiaCooker();
+    }
+}
--- 10.GOF-Altering behavior/Task (Template Method)/Task/TemplateMethod.Task2/MasalaCooker.cs
+++ 10.GOF-Altering behavior/Task (Template Method)/Task/TemplateMethod.Task2/MasalaCooker.cs
@@ -1,20 +1,25 @@
-﻿using System;
-using TemplateMethod.Task2.Cookers;
+﻿using TemplateMethod.Task2.Cookers;
 
 namespace TemplateMethod.Task2
 {
     public class MasalaCooker
     {
-        private ICooker cooker;
+        private readonly ICooker _cooker;
+        private readonly CookerFactory _cookerFactory;
 
         public MasalaCooker(ICooker cooker)
         {
-            this.cooker = cooker;
+            _cooker = cooker;
+            _cookerFactory = new CookerFactory();
         }
 
         public void CookMasala(Country country)
         {
-            throw new NotImplementedException();
+            var localCooker = GetCooker(country);
+            localCooker.CookMasala(_cooker);
         }
+
+        private CookerBase GetCooker(Country country)
+            => country == Country.Ukraine ? _cookerFactory.CreateUkraineCooker() : _cookerFactory.CreateIndianCooker();
     }
 }
